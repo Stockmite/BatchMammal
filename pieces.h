48c48c21d7d73fbe6f7ad5ecc9e960f01af1fc93
Stockmite <rojogreenazul2@gmail.com>
2025-12-18 09:22:39 -0300

    Finished the first function, now onto the pawns

--- pieces.h
+++ pieces.h
@@ -3,15 +3,21 @@
 #define false black
 #define true white
 
-void GetGeneralBoard(int BlackSide[], int WhiteSide[], int GeneralBoard[]) {
+void GetGeneralBoard(int * BlackSide[], int * WhiteSide[], int * GeneralBoard[]) {
 
-
+    for (int x = 0; x < 8; x++)  {
+        for (int y = 0; y < 8; y++) {
+            int IsThereAPiece = BlackSide[x][y] & WhiteSide[x][y];
+            GeneralBoard[x][y] = IsThereAPiece;
+        }
+    }  
 
 
 }
 
-void MovePawn(int * in_pos[], bool which_side, int BlackSide[], int WhiteSide[]) {
+void MovePawn(int * in_pos[], bool which_side, int * BlackSide[], int * WhiteSide[], int * GeneralBoard[]) {
 
+    
     
 
 }
