4f2af2a19ab209b45001376c27f902f83395b06e
Stockmite <rojogreenazul2@gmail.com>
2025-12-18 09:13:46 -0300

    I'll start by making the pieces move

---
+++ pieces.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define false black
+#define true white
+
+void GetGeneralBoard(int BlackSide[], int WhiteSide[], int GeneralBoard[]) {
+
+
+
+
+}
+
+void MovePawn(int * in_pos[], bool which_side, int BlackSide[], int WhiteSide[]) {
+
+    
+
+}
