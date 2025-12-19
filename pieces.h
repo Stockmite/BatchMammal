6cee7836303b39dd9cc76b380edc85793ad91030
Stockmite <rojogreenazul2@gmail.com>
2025-12-19 08:38:50 -0300

    We have 2 new functions: the yin and yan. The option to "create" a piece and the option to "destroy" a piece.

--- pieces.h
+++ pieces.h
@@ -4,6 +4,16 @@
 #define black false
 #define white true
 
+typedef struct {
+    int WSide[8][8];
+    int Bside[8][8];
+    int GenBoard[8][8]; //feels useful enough to define it as a value rather than just rederiving it
+    char PiecePositions[8][8];
+
+} position;
+
+//README: Please treat "true" as 1 and "false" as 0
+
 void GetGeneralBoard(int * BlackSide[], int * WhiteSide[], int * GeneralBoard[]) {
 
     for (int x = 0; x < 8; x++)  {
@@ -16,20 +26,43 @@ void GetGeneralBoard(int * BlackSide[], int * WhiteSide[], int * GeneralBoard[])
 
 }
 
-bool MovePawn(int * in_pos[], bool which_side, int *SSide[], int *GenBoard[], int pawnPos[2]) {
+void Create_Piece(char piece, bool which_side, position * cur_pos, int piecePos[2]) {
+
+    int x = piecePos[0]; int y = piecePos[0];
+    cur_pos->PiecePositions[x][y] = piece;
+    cur_pos->GenBoard[x][y] = true;
+
+    if (which_side == black) {cur_pos->Bside[x][y] = true;}
+    else {cur_pos->WSide[x][y] = true;}
+
+}
+
+void Destroy_Piece(char piece, bool which_side, position * cur_pos, int piecePos[2]) {
+
+    int x = piecePos[0]; int y = piecePos[0];
+    cur_pos->PiecePositions[x][y] = 'a';
+    cur_pos->GenBoard[x][y] = false;
+
+    if (which_side == black) {cur_pos->Bside[x][y] = false;}
+    else {cur_pos->WSide[x][y] = false;}
+
+}
+
+bool MovePawn(bool which_side, position * cur_pos, int pawnPos[2]) {
+    
+    int x = pawnPos[0]; int y = pawnPos[1];
+    if (cur_pos->GenBoard[x][y]) {return false;}
 
     int fileIncrement = 1; if (which_side == black) {fileIncrement *= -1;}
-    int x = pawnPos[0]; int y = pawnPos[1];
+    int newPos[2] = {x, y + fileIncrement};
 
-    if (GenBoard[x][y]) {return false;}
-
-    SSide[x][y], GenBoard[x][y] = false; SSide[x][y + fileIncrement], GenBoard[x][y = fileIncrement] = true;
-    in_pos[x][y] = 'a'; in_pos[x][y + fileIncrement] = 'p';
+    Destroy_Piece()
 
     return true;
 }
 
 bool PawnCapture(int * in_pos[], bool which_side, int *SSide[], int *GenBoard[], int pawnPos[2], int piecePos[2]) {
 
+    
 
 }
