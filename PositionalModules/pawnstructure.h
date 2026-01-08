b8dc2b7f5c7d229d046dedf9d8939b64fbb0dad0
Stockmite <rojogreenazul2@gmail.com>
2026-01-08 08:54:21 -0300

    Almost Finished, just need to do a few extra things

--- PositionalModules/pawnstructure.h
+++ PositionalModules/pawnstructure.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 struct Node {
     int y;
     struct Node *next;
@@ -8,8 +9,27 @@ typedef struct Node PawnNode;
 PawnNode * GetPawn(int PawnPos[2], PawnNode BaseNodes[8], PawnNode ReguNodes[8]) {
     int x = PawnPos[0]; int y = PawnPos[1];
 
-    PawnNode * MainNode = &(BaseNodes[8]);
+    PawnNode * MainNode = &(BaseNodes[x]);
     while (MainNode->y != y) {MainNode = MainNode->next;}
 
     return MainNode;
+}
+
+PawnNode * GetLongPawn(int PawnPos[2], PawnNode BaseNodes[8], PawnNode ReguNodes[8]) {
+    int x = PawnPos[0]; int y = PawnPos[1];
+
+    PawnNode * MainNode = &(BaseNodes[x]);
+    while (MainNode->next != NULL) {MainNode = MainNode->next;}
+
+    return MainNode;
+}
+
+bool DoesFHavePawns(int x, PawnNode BaseNodes[8]) {
+    return (BaseNodes[x].next != NULL);
+}
+
+void MovePawn(int OgPos[2], int NewPos[2], PawnNode BaseNodes[8], PawnNode ReguNodes[8]) {
+
+    
+
 }
